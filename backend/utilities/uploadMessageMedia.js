916cc3eb10ec991f4ded606f09e83f8d4c32e000
unbearabletruth <antaarty@gmail.com>
2023-10-13 12:38:27 +0700

    create upload folder if not exists

--- backend/utilities/uploadMessageMedia.js
+++ backend/utilities/uploadMessageMedia.js
@@ -1,9 +1,13 @@
 const multer = require('multer')
-const path = require('path');
+const fs = require('fs')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/media/')
+    const dir = './public/media'
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir, { recursive: true })
+    }
+    cb(null, dir)
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname) 
